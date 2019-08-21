cd95faab6e17fe18c7054407872f3cffcc6c5098
Ananthu Kanive <ananthu.kanive@ymedialabs.com>
2019-08-21 15:15:25 +0530

    add basic ui

--- app/src/main/java/com/example/curcurrent/MainActivity.kt
+++ app/src/main/java/com/example/curcurrent/MainActivity.kt
@@ -2,11 +2,46 @@ package com.example.curcurrent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.curcurrent.model.Currency
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
+
+    var ratesAPI = RatesAPI.create()
+    var subscribe : Disposable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        subscribe = ratesAPI.currentRates()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .map{
+                val rateTable = ArrayList<Currency>()
+                for(rate in it.rates){
+                    rateTable.add(
+                        Currency(rate.key, "test", rate.value)
+                    )
+                }
+                rateTable
+            }
+            .subscribe(
+                { result ->
+                    run {
+                        Log.d("api", result.toString())
+                    }
+                },
+                { error -> Log.d("api", error.localizedMessage) }
+            )
+
+    }
+
+    override fun onDestroy() {
+        subscribe?.dispose()
+        super.onDestroy()
     }
 }
